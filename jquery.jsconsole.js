6be47e634a6ab24c6a180b0907075d6c3649d968
godspeed1715 <ruoangelus@gmail.com>
2014-01-20 22:34:04 -0600

    Major update.
    Added a console that will eval commands, and a history on that command.

--- jquery.jsconsole.js
+++ jquery.jsconsole.js
@@ -1,5 +1,7 @@
 var jsConsole = {
     timerNames: [],
+    cmdHistory: [],
+    cmdHistoryPosition: "",
      cssPanelBox: {
         'width': '100%',
         'color': '#000',
@@ -40,6 +42,12 @@ var jsConsole = {
         'color': '#ededed',
         'border-top': '1px solid #cacaca'
     },
+     cssCommandLine: {
+        'width': '95%',
+        'padding':'5px',
+        'border': 'none',
+        'outline-width': '0px'
+    },
     isOn: false,
     init: function () {
         if ($("panel-box")) {
@@ -52,15 +60,20 @@ var jsConsole = {
                 '       <span id="panel-menu-minimize" onclick="jsConsole.minimize();">Minimize</span> ' +
                 '       <span id="panel-menu-close" style="float: right; margin-right: 5px; cursor: pointer; color: grey;"><i class="fa fa-times"></i></span> ' +
                 '   </div>' + 
-                '  <div id="panel-console"></div>' +
-                '  <div id="panel-bottom-nav"></div>' +
+                '   <div id="panel-console"></div>' +
+                '  <span id="panel-commandline">' +
+                '       <span><i class="fa fa-chevron-right"></i></span><input id="commandline">' +
+                '   </span>' +
+                '   <div id="panel-bottom-nav"></div>' +
                 '</div>';
             var consolePanel = $(layout);
             $('body').append(consolePanel);
             $("#panel-box").css(this.cssPanelBox);
             $('#panel-dragbar').css(this.cssPanelDragBar);
-            $('#panel-header').css(this.cssPanelHeader);
+            $('#panel-header').css(this.cssPanelHeader)
+                .find('span').css('cursor','pointer');
             $("#panel-console").css(this.cssPanelConsole);
+            $('#panel-commandline, #commandline').css(this.cssCommandLine);
             $("#panel-bottom-nav").css(this.cssPanelNavigation);
         };
         //Drapbar to expand panel-consle div
@@ -76,13 +89,11 @@ var jsConsole = {
         });
 
         document.addEventListener('keydown', function(event) {
-            var keyCode = event.which || event.keycode;
-            if (keyCode == 123) { //F12 key is 123.
-                jsConsole.toggle(); //call toggle function.
+            if (event.keyCode == 123) {
+                jsConsole.toggle();
             }
         }, true);
         $('#panel-menu-close').bind({
-            //Add a hover event for the close "x" in the panel header.
             mouseenter: function () {
                 $(this).find('i').css("color", "black");
             },
@@ -90,46 +101,128 @@ var jsConsole = {
                 $(this).find('i').css("color", "grey");
             },
             click: function () {
-                jsConsole.toggle(); //call toggle function.
+                jsConsole.toggle();
             }
         });
-        (function() {
-          if (!window.console) {
-            window.console = {};
-          }
-          console = window || window.console;
+       $('#commandline').on('keypress', function (event) {
+           if ( event.which == 13) {
+                var sCmd = $('#commandline').val();
+                if (sCmd == "clear") {
+                    jsConsole.clear();
+                     jsConsole.cmdHistory.push(sCmd);
+                    $('#commandline').val('');
+                } else if (sCmd === "history"){
+                    for (var x = 0; x < jsConsole.cmdHistory.length; x++) {
+                        console.log(jsConsole.cmdHistory[x]);
+                    }
+                    $('#commandline').val('');
+                } else if (sCmd) {
+                     jsConsole.log('<span><i class="fa fa-chevron-right"></i> <span stle="margin-left: 18px;">' + sCmd +'</span>');
+                     jsConsole.cmdHistory.push(sCmd);
+                    try {
+                        var evalCmd = eval(sCmd);
+                        console.log(evalCmd);
+                        $('#commandline').val('');
+                    } catch(event) {
+                        $('#commandline').val('');
+                        throw event;
+                    }
+                }
+                jsConsole.cmdHistoryPosition = jsConsole.cmdHistory.length;
+             } 
+        }).on('keydown', function (event) {
+            if (event.which === 38) {
+                jsConsole.cmdHistoryPosition--
+                if (jsConsole.cmdHistoryPosition < 0) jsConsole.cmdHistoryPosition = 0; 
+                if (jsConsole.cmdHistory[jsConsole.cmdHistoryPosition] != undefined && jsConsole.cmdHistory[jsConsole.cmdHistoryPosition] !== '') {
+                    $('#commandline').val(jsConsole.cmdHistory[jsConsole.cmdHistoryPosition]);
+                    return false;
+                } else if (jsConsole.cmdHistoryPosition == jsConsole.cmdHistory.length) {
+                    $('#commandline').val('');
+                    return false;
+                }
+            } else if (event.which == 40) {
+                jsConsole.cmdHistoryPosition++
+                if (jsConsole.cmdHistoryPosition >= jsConsole.cmdHistory.length) jsConsole.cmdHistoryPosition = jsConsole.cmdHistory.length; //0
+                if (jsConsole.cmdHistory[jsConsole.cmdHistoryPosition] != undefined && jsConsole.cmdHistory[jsConsole.cmdHistoryPosition] !== '') {
+                    $('#commandline').val(jsConsole.cmdHistory[jsConsole.cmdHistoryPosition]);
+                    return false;
+                } else if (jsConsole.cmdHistoryPosition == jsConsole.cmdHistory.length) {
+                    $('#commandline').val('');
+                    return false;
+                }            
+            }
+        })
+        $('#panel-console').on('click', function () {
+          $('#commandline').focus();
+        });
+          
+          console = window //|| window.console;
           // union of Chrome, FF, IE, and Safari console methods
           var method = [
             "log", "info", "warn", "onerror", "debug", "trace", "dir", "group",
             "groupCollapsed", "groupEnd", "time", "timeEnd", "profile", "profileEnd",
-            "dirxml", "assert", "count", "markTimeline", "timeStamp", "clear"
+            "dirxml", "assert", "count", "markTimeline", "timeStamp", "clear", "cmd"
           ];
           // define undefined methods as noops to prevent errors
           for (var i = 0; i < method.length; i++) {
             if (!window.console[method[i]]) {
                 console[method[i]] = function() {
-
                     var sArguments = "";
                     for (var i = 0, j = arguments.length; i < j; i++){
-                    sArguments += arguments[i] + " ";
-                }
-                if (method[i] === "onerror") {
-                    var scriptNameURL = arguments[1]
-                    if (scriptNameURL.match('/') || scriptNameURL.match('\\')) {
-                        ExplodescriptName = scriptNameURL.split('/') || scriptNameURL.split('\\')
-                        scriptName = ExplodescriptName[ExplodescriptName.length - 1];
-                    } else {
-                        scriptName = arguments[1];
+                        sArguments += arguments[i] + " ";
                     }
-                    jsConsole.log('<span style="color: red;"><i class="fa fa-times-circle"></i> <span>' + arguments[0] +'</span> <a href="' + scriptNameURL +'" class="pull-right">' + scriptName + ":" + arguments[2] + '</a></span>');
-                } else {
-                    jsConsole.log('<i class="fa fa-info-circle"></i> <span>' + sArguments + "</span>");
-                }
-                return true;
+                    if (method[i] === "onerror") {
+                        var scriptNameURL = arguments[1]
+                        if (scriptNameURL.match('/') || scriptNameURL.match('\\')) {
+                            ExplodescriptName = scriptNameURL.split('/') || scriptNameURL.split('\\')
+                            scriptName = ExplodescriptName[ExplodescriptName.length - 1];
+                        } else {
+                            scriptName = arguments[1];
+                        }
+                        jsConsole.log('<span style="color: red;"><i class="fa fa-times-circle"></i> <span>' + arguments[0] +'</span> <a href="' + scriptNameURL +'" class="pull-right">' + scriptName + ":" + arguments[2] + '</a></span>');
+                    } 
+                    return true;
               };
             }    
           } 
-        })();
+          console = {
+            clear: function () {
+                jsConsole.clear();
+            }, 
+            cmd: function (arg) {
+                jsConsole.log('<i class="fa fa-chevron-right"></i><span>' + arg + "</span>");
+            }, 
+            info: function (arg) {
+                jsConsole.log('<i class="fa fa-info-circle"></i> <span>' + arg + "</span>");
+            },
+            log: function (arg) {
+                jsConsole.log('<span style="padding-left: 18px;">' + arg + '</span>')
+            },
+            warn: function (arg) {
+                jsConsole.log('<span style="color: yellow;"><i class="fa fa-warning"></i></span> <span>' + arg + '</span>')
+            },
+            error: function (arg) {
+                throw arg;
+            },
+            time: function (sTimerName) {
+              jsConsole.timerNames.push({
+                sTimerName : sTimerName,
+                dStartTime: jsConsole.now()
+              });
+            },
+            timeEnd: function (sTimerName) {
+              if (jsConsole.timerNames.length !== 0 ) { 
+                var dEndTime = jsConsole.now();
+                for (var x = 0; x< jsConsole.timerNames.length; x++) {
+                  if (jsConsole.timerNames[x].sTimerName === sTimerName) {
+                     var TimerNameEnd = dEndTime - parseInt(jsConsole.timerNames[x].dStartTime, 10);
+                    jsConsole.log('<i class="fa fa-clock-o"></i> <span>' + JSON.stringify(jsConsole.timerNames[x].sTimerName).replace(/"/g,"") + ': ' + TimerNameEnd + '</span>');
+                  }
+                }
+              }
+            }
+        }
         this.isOn = true;
     },
     log: function (msg, pause) {
@@ -181,7 +274,7 @@ var jsConsole = {
         this.isOn = false;
     },
     clear: function () {
-        $("#panel-console").html("");
+        $("#panel-console").text("");
     },
     onOff: function () {
         if (this.isOn === false) {
