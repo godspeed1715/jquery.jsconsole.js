0779fe2b504f5d2fac79d769c2f3734201c233ce
godspeed1715 <ruoangelus@gmail.com>
2014-01-20 22:49:41 -0600

    prettied up.

--- jquery.jsconsole.js
+++ jquery.jsconsole.js
@@ -2,7 +2,7 @@ var jsConsole = {
     timerNames: [],
     cmdHistory: [],
     cmdHistoryPosition: "",
-     cssPanelBox: {
+    cssPanelBox: {
         'width': '100%',
         'color': '#000',
         'font-family': 'Courier',
@@ -26,14 +26,14 @@ var jsConsole = {
         'border-top': '1px solid #fff',
         'border-bottom': '1px solid #cacaca',
         'text-align': 'left',
-        'font-family': 'Arial'  
+        'font-family': 'Arial'
     },
     cssPanelConsole: {
         'overflow': 'auto',
         'height': '150px',
         'padding': ' 0 5px 0 5px',
         'text-align': 'left',
-        'background':'#fff'
+        'background': '#fff'
     },
     cssPanelNavigation: {
         'height': '20px',
@@ -42,16 +42,16 @@ var jsConsole = {
         'color': '#ededed',
         'border-top': '1px solid #cacaca'
     },
-     cssCommandLine: {
+    cssCommandLine: {
         'width': '95%',
-        'padding':'5px',
+        'padding': '5px',
         'border': 'none',
         'outline-width': '0px'
     },
     isOn: false,
     init: function () {
         if ($("panel-box")) {
-            var layout =  '<div id="panel-box">' +
+            var layout = '<div id="panel-box">' +
                 '  <div id="panel-dragbar"></div>' +
                 '  <div id="panel-header">' +
                 '       <span onclick="jsConsole.onOff();" id="panel-menu-disable">Disable</span> ' +
@@ -59,7 +59,7 @@ var jsConsole = {
                 '       <span id="panel-menu-transparent" onclick="jsConsole.transparent();">Transparent</span> ' +
                 '       <span id="panel-menu-minimize" onclick="jsConsole.minimize();">Minimize</span> ' +
                 '       <span id="panel-menu-close" style="float: right; margin-right: 5px; cursor: pointer; color: grey;"><i class="fa fa-times"></i></span> ' +
-                '   </div>' + 
+                '   </div>' +
                 '   <div id="panel-console"></div>' +
                 '  <span id="panel-commandline">' +
                 '       <span><i class="fa fa-chevron-right"></i></span><input id="commandline">' +
@@ -71,7 +71,7 @@ var jsConsole = {
             $("#panel-box").css(this.cssPanelBox);
             $('#panel-dragbar').css(this.cssPanelDragBar);
             $('#panel-header').css(this.cssPanelHeader)
-                .find('span').css('cursor','pointer');
+                .find('span').css('cursor', 'pointer');
             $("#panel-console").css(this.cssPanelConsole);
             $('#panel-commandline, #commandline').css(this.cssCommandLine);
             $("#panel-bottom-nav").css(this.cssPanelNavigation);
@@ -88,7 +88,7 @@ var jsConsole = {
             $(document).unbind('mousemove');
         });
 
-        document.addEventListener('keydown', function(event) {
+        document.addEventListener('keydown', function (event) {
             if (event.keyCode == 123) {
                 jsConsole.toggle();
             }
@@ -104,36 +104,40 @@ var jsConsole = {
                 jsConsole.toggle();
             }
         });
-       $('#commandline').on('keypress', function (event) {
-           if ( event.which == 13) {
+        $('#commandline').on('keypress', function (event) {
+            if (event.which == 13) {
                 var sCmd = $('#commandline').val();
                 if (sCmd == "clear") {
                     jsConsole.clear();
-                     jsConsole.cmdHistory.push(sCmd);
+                    jsConsole.cmdHistory.push(sCmd);
                     $('#commandline').val('');
-                } else if (sCmd === "history"){
+                } else if (sCmd === "history") {
                     for (var x = 0; x < jsConsole.cmdHistory.length; x++) {
                         console.log(jsConsole.cmdHistory[x]);
                     }
                     $('#commandline').val('');
                 } else if (sCmd) {
-                     jsConsole.log('<span><i class="fa fa-chevron-right"></i> <span stle="margin-left: 18px;">' + sCmd +'</span>');
-                     jsConsole.cmdHistory.push(sCmd);
+                    jsConsole.log('<span><i class="fa fa-chevron-right"></i> <span stle="margin-left: 18px;">' + sCmd + '</span>');
+                    jsConsole.cmdHistory.push(sCmd);
                     try {
                         var evalCmd = eval(sCmd);
-                        console.log(evalCmd);
+                        if (typeof evalCmd === "object") {
+                            console.log(JSON.stringify(evalCmd))
+                        } else {
+                            console.log(evalCmd);
+                        }
                         $('#commandline').val('');
-                    } catch(event) {
+                    } catch (event) {
                         $('#commandline').val('');
                         throw event;
                     }
                 }
                 jsConsole.cmdHistoryPosition = jsConsole.cmdHistory.length;
-             } 
+            }
         }).on('keydown', function (event) {
             if (event.which === 38) {
                 jsConsole.cmdHistoryPosition--
-                if (jsConsole.cmdHistoryPosition < 0) jsConsole.cmdHistoryPosition = 0; 
+                if (jsConsole.cmdHistoryPosition < 0) jsConsole.cmdHistoryPosition = 0;
                 if (jsConsole.cmdHistory[jsConsole.cmdHistoryPosition] != undefined && jsConsole.cmdHistory[jsConsole.cmdHistoryPosition] !== '') {
                     $('#commandline').val(jsConsole.cmdHistory[jsConsole.cmdHistoryPosition]);
                     return false;
@@ -150,26 +154,26 @@ var jsConsole = {
                 } else if (jsConsole.cmdHistoryPosition == jsConsole.cmdHistory.length) {
                     $('#commandline').val('');
                     return false;
-                }            
+                }
             }
         })
         $('#panel-console').on('click', function () {
-          $('#commandline').focus();
+            $('#commandline').focus();
         });
-          
-          console = window //|| window.console;
-          // union of Chrome, FF, IE, and Safari console methods
-          var method = [
+
+        console = window //|| window.console;
+        // union of Chrome, FF, IE, and Safari console methods
+        var method = [
             "log", "info", "warn", "onerror", "debug", "trace", "dir", "group",
             "groupCollapsed", "groupEnd", "time", "timeEnd", "profile", "profileEnd",
             "dirxml", "assert", "count", "markTimeline", "timeStamp", "clear", "cmd"
-          ];
-          // define undefined methods as noops to prevent errors
-          for (var i = 0; i < method.length; i++) {
+        ];
+        // define undefined methods as noops to prevent errors
+        for (var i = 0; i < method.length; i++) {
             if (!window.console[method[i]]) {
-                console[method[i]] = function() {
+                console[method[i]] = function () {
                     var sArguments = "";
-                    for (var i = 0, j = arguments.length; i < j; i++){
+                    for (var i = 0, j = arguments.length; i < j; i++) {
                         sArguments += arguments[i] + " ";
                     }
                     if (method[i] === "onerror") {
@@ -180,19 +184,19 @@ var jsConsole = {
                         } else {
                             scriptName = arguments[1];
                         }
-                        jsConsole.log('<span style="color: red;"><i class="fa fa-times-circle"></i> <span>' + arguments[0] +'</span> <a href="' + scriptNameURL +'" class="pull-right">' + scriptName + ":" + arguments[2] + '</a></span>');
-                    } 
+                        jsConsole.log('<span style="color: red;"><i class="fa fa-times-circle"></i> <span>' + arguments[0] + '</span> <a href="' + scriptNameURL + '" class="pull-right">' + scriptName + ":" + arguments[2] + '</a></span>');
+                    }
                     return true;
-              };
-            }    
-          } 
-          console = {
+                };
+            }
+        }
+        console = {
             clear: function () {
                 jsConsole.clear();
-            }, 
+            },
             cmd: function (arg) {
                 jsConsole.log('<i class="fa fa-chevron-right"></i><span>' + arg + "</span>");
-            }, 
+            },
             info: function (arg) {
                 jsConsole.log('<i class="fa fa-info-circle"></i> <span>' + arg + "</span>");
             },
@@ -206,22 +210,22 @@ var jsConsole = {
                 throw arg;
             },
             time: function (sTimerName) {
-              jsConsole.timerNames.push({
-                sTimerName : sTimerName,
-                dStartTime: jsConsole.now()
-              });
+                jsConsole.timerNames.push({
+                    sTimerName: sTimerName,
+                    dStartTime: jsConsole.now()
+                });
             },
             timeEnd: function (sTimerName) {
-              if (jsConsole.timerNames.length !== 0 ) { 
-                var dEndTime = jsConsole.now();
-                for (var x = 0; x< jsConsole.timerNames.length; x++) {
-                  if (jsConsole.timerNames[x].sTimerName === sTimerName) {
-                    var TimerNameEnd = dEndTime - parseInt(jsConsole.timerNames[x].dStartTime, 10);
-                    jsConsole.log('<i class="fa fa-clock-o"></i> <span>' + JSON.stringify(jsConsole.timerNames[x].sTimerName).replace(/"/g,"") + ': ' + TimerNameEnd + '</span>');
-                    jsConsole.timerNames[x] = "";//temp fix to remove timer name.
-                  }
+                if (jsConsole.timerNames.length !== 0) {
+                    var dEndTime = jsConsole.now();
+                    for (var x = 0; x < jsConsole.timerNames.length; x++) {
+                        if (jsConsole.timerNames[x].sTimerName === sTimerName) {
+                            var TimerNameEnd = dEndTime - parseInt(jsConsole.timerNames[x].dStartTime, 10);
+                            jsConsole.log('<i class="fa fa-clock-o"></i> <span>' + JSON.stringify(jsConsole.timerNames[x].sTimerName).replace(/"/g, "") + ': ' + TimerNameEnd + '</span>');
+                            jsConsole.timerNames[x] = ""; //temp fix to remove timer name.
+                        }
+                    }
                 }
-              }
             }
         }
         this.isOn = true;
@@ -239,34 +243,36 @@ var jsConsole = {
     consoleresize: function () {
         $("#panel-console").scrollTop($("#panel-console")[0].scrollHeight);
     },
-      now: function () {
-        var performance = window.performance ||{};
+    now: function () {
+        var performance = window.performance || {};
         performance.now = (function () {
-            return performance.now    ||
-            performance.webkitNow     ||
-            performance.msNow         ||
-            performance.oNow          ||
-            performance.mozNow        ||
-            function () { return new Date().getTime(); };
+            return performance.now ||
+                performance.webkitNow ||
+                performance.msNow ||
+                performance.oNow ||
+                performance.mozNow ||
+                function () {
+                    return new Date().getTime();
+            };
         })();
         return performance.now();
-      },
+    },
     time: function (sTimerName) {
-      this.timerNames.push({
-        sTimerName : sTimerName,
-        dStartTime: this.now()
-      });
+        this.timerNames.push({
+            sTimerName: sTimerName,
+            dStartTime: this.now()
+        });
     },
     timeEnd: function (sTimerName) {
-      if (this.timerNames.length !== 0 ) { 
-        var dEndTime = this.now();
-        for (var x = 0; x< this.timerNames.length; x++) {
-          if (this.timerNames[x].sTimerName === sTimerName) {
-             var TimerNameEnd = dEndTime - parseInt(this.timerNames[x].dStartTime, 10);
-            this.log('<i class="fa fa-clock-o"></i> <span>' + JSON.stringify(this.timerNames[x].sTimerName).replace(/"/g,"") + ' ' + TimerNameEnd + '</span>');
-          }
+        if (this.timerNames.length !== 0) {
+            var dEndTime = this.now();
+            for (var x = 0; x < this.timerNames.length; x++) {
+                if (this.timerNames[x].sTimerName === sTimerName) {
+                    var TimerNameEnd = dEndTime - parseInt(this.timerNames[x].dStartTime, 10);
+                    this.log('<i class="fa fa-clock-o"></i> <span>' + JSON.stringify(this.timerNames[x].sTimerName).replace(/"/g, "") + ' ' + TimerNameEnd + '</span>');
+                }
+            }
         }
-      }
     },
     enable: function () {
         this.isOn = true;
@@ -281,8 +287,7 @@ var jsConsole = {
         if (this.isOn === false) {
             this.enable();
             $("#panel-menu-disable").html("Disable");
-        }
-        else {
+        } else {
             this.disable();
             $("#panel-menu-disable").html("Enable");
 
@@ -292,8 +297,7 @@ var jsConsole = {
         if ($("#panel-box").css("opacity") === "0.5") {
             $("#panel-box").css("opacity", "1.0");
             $("#panel-menu-transparent").html("Transparent");
-        }
-        else {
+        } else {
             $("#panel-box").css("opacity", "0.5");
             $("#panel-menu-transparent").html("Opaque");
         }
@@ -306,8 +310,7 @@ var jsConsole = {
                 "display": "none"
             });
             $("#panel-menu-minimize").html("Expand");
-        }
-        else {
+        } else {
             $("#panel-console").css({
                 "height": this.panelConsoleHeight,
                 "display": "block"
@@ -319,12 +322,9 @@ var jsConsole = {
     toggle: function () {
         if ($("#panel-box").css("display") == "block") {
             $("#panel-box").css("display", "none");
-        }
-        else {
+        } else {
             $("#panel-box").css("display", "block");
             this.consoleresize();
         }
-  }
+    }
 };
-
-
